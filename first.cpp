ada669e43d3e23ecf9d61fa7bdff51035c6e6345
NARooshnavand <nasserakhoundi@gmail.com>
2024-03-03 05:16:39 -0800

    first commit on sesssion 3 at 13-12-1402

---
+++ first.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std; 
+
+int main(){
+    char c=66, v='B';
+    int n = 66;
+    
+    cout << c << " equals with " << v << " But not equal with " << n;
+    return 0;
+}
