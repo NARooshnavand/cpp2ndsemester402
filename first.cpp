9a98216aa083ccb35e394b192c408f4313aa8e82
NARooshnavand <nasserakhoundi@gmail.com>
2024-03-03 05:31:37 -0800

    second commit on sesssion 3 at 13-12-1402

--- first.cpp
+++ first.cpp
@@ -5,7 +5,13 @@ using namespace std;
 int main(){
     char c=66, v='B';
     int n = 66;
-    
-    cout << c << " equals with " << v << " But not equal with " << n;
+    bool a = false;
+    if(a){
+        cout << " a is true";
+    }else{
+        cout << " a is not true";   
+        a= true;
+    }
+    a? cout << " a is true" : cout << "a is not true";
     return 0;
 }
