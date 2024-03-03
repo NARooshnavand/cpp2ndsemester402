c017d6ff243dd6a944e3f5403eb1c484f246cc81
NARooshnavand <nasserakhoundi@gmail.com>
2024-03-03 06:09:46 -0800

    fourth commit on sesssion 3 at 13-12-1402

--- first.cpp
+++ first.cpp
@@ -1,21 +1,7 @@
 #include<iostream>
-
+#include "Account.h"
 using namespace std; 
-int sum(int a, int b){
-    return a+b;
-}
-float sum(float a, float b){
-    return a+b;
-}
 int main(){
-    char c{66}, v{'B'};
-    int m{54}, n;
-    float f1{1.2}, f2{54.3};
-    double d1;
-    n = 62;
-    d1 = 1.321e34;
-    double d2 = 4.25e12,d3;
-    d3 = d1/d2;
-    cout << d3;
+    Account account('B');    
     return 0;
 }
