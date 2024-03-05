bd784d3ba81c6f2626661e7dc24725f78bd0aff2
NARooshnavand <nasserakhoundi@gmail.com>
2024-03-04 23:04:46 -0800

    first commit session 15 - 12 - 1402

--- Account.h
+++ Account.h
@@ -1,7 +1,17 @@
 #include<iostream>
 class Account{
     public:
-    Account(char st1){
-        std::cout<< "This is first class : " << st1;
+    // Method , here is constructor 
+    Account(int b){
+        if(b> 5000)
+            balance = b;
     }
+    //Another method in c++ member function 
+    int getBalance(){
+        return balance;
+    }
+    int balance{5000};
+    // private:
+    // //Feild of class , in c++ member attribute
+    //     int balance{5000};
 };
--- first.cpp
+++ first.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 #include "Account.h"
-using namespace std; 
+using namespace std;
 int main(){
-    Account account('B');    
+    Account account(7000);   
+    cout << account.getBalance()<<endl; 
+    account.balance = 10000;
+    cout << account.getBalance()<<endl; 
+
     return 0;
 }
